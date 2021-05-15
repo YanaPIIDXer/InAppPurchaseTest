7f48852f68c8dfd0d3925b5254c1dfadc1eb2628
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-15 19:56:03 +0900

    UI管理クラスをとりあえず定義

---
+++ Client/InAppPurchaseTest/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// UI管理
+/// </summary>
+public class UIManager : MonoBehaviour
+{
+    /// <summary>
+    /// インスタンス
+    /// </summary>
+    public static UIManager Instance { get; private set; }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+    }
+}
