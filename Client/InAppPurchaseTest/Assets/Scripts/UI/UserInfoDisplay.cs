769e3aebe7eb664fa4a403d5293bee3923b8abd1
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-15 13:30:20 +0900

    ユーザ情報表示

---
+++ Client/InAppPurchaseTest/Assets/Scripts/UI/UserInfoDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UniRx;
+
+/// <summary>
+/// ユーザ情報表示
+/// </summary>
+public class UserInfoDisplay : MonoBehaviour
+{
+    /// <summary>
+    /// 金表示
+    /// </summary>
+    [SerializeField]
+    private Text GoldText = null;
+
+    /// <summary>
+    /// SPモード表示
+    /// </summary>
+    [SerializeField]
+    private Text SPModeText = null;
+
+    void Awake()
+    {
+        UserData.Instance.OnUpdateGold
+            .Subscribe((Value) => GoldText.text = string.Format("Gold: {0}", Value))
+            .AddTo(gameObject);
+
+        UserData.Instance.OnUpdateSPMode
+            .Subscribe((Value) => SPModeText.text = string.Format("SPMode: {0}", Value))
+            .AddTo(gameObject);
+    }
+}
