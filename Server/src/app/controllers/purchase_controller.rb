051824b9be3786e986c9d540e6c9d79cdfe81300
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-16 00:25:57 +0900

    セッションがない場合は問答無用で失敗を返すようにした

--- Server/src/app/controllers/purchase_controller.rb
+++ Server/src/app/controllers/purchase_controller.rb
@@ -1,5 +1,18 @@
 class PurchaseController < ApplicationController
+  before_action :check_session
+  skip_before_action :check_session, only: [:session_fail]
+  
   def add_money
-    render json => { success: true, money: 9999 }
+    render :json => { success: true, money: 9999 }
+  end
+
+  def session_fail
+    render :json => { success: false }
+  end
+
+  def check_session
+    if session[:id] != nil
+      redirect_to action: :session_fail
+    end
   end
 end
--- Server/src/config/routes.rb
+++ Server/src/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "health_check#index"
   post "/login" => "login#index"
   post "/purchase/add_money" => "purchase#add_money"
+  post "/purchase/session_fail" => "purchase#session_fail"
 end
