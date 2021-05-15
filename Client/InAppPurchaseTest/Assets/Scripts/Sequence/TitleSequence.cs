ebd4adc12d0f93c66181564f262b33ca2b475e0e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-15 13:12:17 +0900

    ログイン成功時にメニュー画面に飛ばすようにした

---
+++ Client/InAppPurchaseTest/Assets/Scripts/Sequence/TitleSequence.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// タイトルシーケンス
+/// </summary>
+public class TitleSequence : MonoBehaviour
+{
+    /// <summary>
+    /// ログイン
+    /// </summary>
+    public void Login()
+    {
+        // とりあえず面倒なので名前はダミーのものを突っ込む
+        string DummyName = "Test";
+
+        StartCoroutine(APICall.Login(DummyName, (Result) =>
+        {
+            if (!Result.result)
+            {
+                Debug.Log("Login Failed.");
+                return;
+            }
+
+            SceneManager.LoadScene("Menu");
+        }));
+    }
+}
