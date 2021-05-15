22a93428174cbe3176b37f92b9e34e139c311dc4
YanaPIIDXer <>
2021-05-15 21:14:29 +0900

    商品購入インタフェース定義

---
+++ Client/InAppPurchaseTest/Assets/Scripts/Purchase/IProductPurchase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+/// <summary>
+/// 商品購入インタフェース
+/// </summary>
+public interface IProductPurchase
+{
+    /// <summary>
+    /// ストアコントローラ
+    /// </summary>
+    IStoreController StoreController { set; }
+}
