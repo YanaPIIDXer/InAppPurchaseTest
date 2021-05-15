014718b38a4dd467fca2888d32adb0716e9a6b7e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-15 19:00:57 +0900

    とりあえずストアの初期化 ※商品情報がまだ未定義なので失敗する

---
+++ Client/InAppPurchaseTest/Assets/Scripts/Purchase/Store.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+/// <summary>
+/// ストアクラス
+/// </summary>
+public class Store : MonoBehaviour
+{
+    /// <summary>
+    /// イベントリスナ
+    /// </summary>
+    private StoreListener Listener = new StoreListener();
+
+    void Awake()
+    {
+        var PurchaseModule = StandardPurchasingModule.Instance();
+        PurchaseModule.useFakeStoreUIMode = FakeStoreUIMode.StandardUser;
+        var Builder = ConfigurationBuilder.Instance(PurchaseModule);
+        UnityPurchasing.Initialize(Listener, Builder);
+    }
+}
---
+++ Client/InAppPurchaseTest/Assets/Scripts/Purchase/StoreListener.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+/// <summary>
+/// 課金系のイベントリスナ
+/// </summary>
+public class StoreListener : IStoreListener
+{
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+        Debug.LogError("Store Initialize Failed.");
+        Debug.LogError("Reason:" + error.ToString());
+    }
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    {
+        Debug.LogError("Purchase Failed. ID:" + product.definition.id);
+        Debug.LogError("Reason:" + failureReason.ToString());
+    }
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs purchaseEvent)
+    {
+        return PurchaseProcessingResult.Complete;
+    }
+}
